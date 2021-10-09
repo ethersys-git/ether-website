8dd6c2573ec1c8c1a3a51ef2a8bd8e716d0a2263
VishalJoshi <yourvishal007@gmail.com>
2021-10-09 10:21:29 +0530

    updates

--- src/containers/homepage/topSection.jsx
+++ src/containers/homepage/topSection.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { Element, scroller } from "react-scroll";
 import styled from "styled-components";
 
-import BackgroundImg from "../../assets/pictures/company_team.jpg";
-import { Button } from "../../components/button";
-import { DownArrow } from "../../components/downArrow";
-import { Logo } from "../../components/logo";
-import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navbar";
 import { Carousel } from "react-responsive-carousel";
+import {
+  carousel1,
+  carousel2,
+  carousel3,
+  carousel4,
+  carousel5,
+  carousel6,
+} from "../../assets/images";
 
 const TopContainer = styled.div`
   width: 100%;
   height: 100vh;
   padding: 0;
-  background-image: url(${BackgroundImg});
   position: relative;
 `;
 
@@ -47,25 +49,37 @@ export function TopSection(props) {
   const scrollToNextSection = () => {
     scroller.scrollTo("servicesSection", { smooth: true, duration: 1500 });
   };
+  const images = [
+    carousel1,
+    carousel2,
+    carousel3,
+    carousel4,
+    carousel5,
+    carousel6,
+  ];
 
   return (
     <Element name="topSection">
       <TopContainer>
-        <BackgroundFilter>
-          <Navbar />
-          <Marginer direction="vertical" margin="8em" />
-          <Logo />
-          <Marginer direction="vertical" margin="4em" />
+        <Navbar />
+        <Carousel autoPlay showThumbs={false}>
+          {images.map((item) => {
+            return (
+              <img src={item} style={{ width: "100%", height: "100vh" }} />
+            );
+          })}
+        </Carousel>
+        {/* <Marginer direction="vertical" margin="8em" />
+          <Logo /> */}
+        {/* <Marginer direction="vertical" margin="4em" />
           <MotivationalText>Software Development</MotivationalText>
           <MotivationalText>From the Best in the Industry</MotivationalText>
-          <Marginer direction="vertical" margin="2em" />
-          
+          <Marginer direction="vertical" margin="2em" /> */}
 
-          <Button>Start your Project</Button>
+        {/* <Button>Start your Project</Button>
           <DownArrowContainer onClick={scrollToNextSection}>
             <DownArrow />
-          </DownArrowContainer>
-        </BackgroundFilter>
+          </DownArrowContainer> */}
       </TopContainer>
     </Element>
   );
